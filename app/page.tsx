572b0416d3060d384878e709203ebc864a0d633d
Alex Dea <alex.anderdea@hotmail.com>
2026-06-25 15:34:32 -0600

    Setup tailwinds and did initial styling on  page.txs. will test later

--- app/page.tsx
+++ app/page.tsx
@@ -15,14 +15,28 @@ export default function Home() {
   }
   return (
     <>
+    <div className="bg-black-50 min-h-screen">
       <Navbar />
-      <main>
-        {Studentlist.map((student) => (
-          <StudentCard key={student.id} first={student.firstName} last={student.lastName} dob={student.dob} grade={student.grade} />
-        ))}
-        <FormDisplay addstudent={addstudent} />
+      <main className="max-w-4xl p-8">
+        <div className="border-b border-black-400 pb-2 mb-4">
+          <h1 className="font-bold text-black-800">List of Students</h1>
+        </div>
+        <div className="flex flex-col gap-15">
+          <div className="felx flex-col gap-4">
+            {Studentlist.map((student) => (
+            <StudentCard key={student.id} first={student.firstName} last={student.lastName} dob={student.dob} grade={student.grade} />
+             ))}
+          </div>
+          
+        </div>
+        <div className="border-b border-black-400 pb-2 mb-4">
+          <h2 className="font-bold text-black-800">Register a New Student</h2>
+         <FormDisplay addstudent={addstudent} />
+        </div>
       </main>
       <Footer />
+    </div>
+      
     </>
   );
 }
