9e095501e11e719555277923f8b1683a79630a6e
AlexDeaDev <alex.anderdea@hotmail.com>
2026-06-26 20:46:50 -0600

    Created card styling + added addStudent function (this needs a little testing)

--- app/components/studentcard.tsx
+++ app/components/studentcard.tsx
@@ -5,8 +5,11 @@ export default function StudentCard({ first, last, dob, grade }:
         dob: string;
         grade: number;
     }) {
+
+
+    const cardStyle = "bg-gray-100 p-4 border border-black rounded-lg shadow-md flex flex-col space-y-2 max-w-3xs";
     return (
-        <div>
+        <div className={cardStyle}>
             <p>First Name: {first}</p>
             <p>Last Name: {last}</p>
             <p>Date of Birth: {dob}</p>
--- app/page.tsx
+++ app/page.tsx
@@ -10,31 +10,36 @@ import students from "./Studentlist";
 export default function Home() {
   const [Studentlist, setStudents] = useState(students);
 
-  function addstudent() {
+  function addstudent(studentData: { first: string; last: string; dob: string; grade: number }) {
 
+    const newId = Studentlist.length + 1;
+    const newStudent = {
+      id: newId,
+      firstName: studentData.first,
+      lastName: studentData.last,
+      dob: studentData.dob,
+      grade: studentData.grade,
+    };
+    setStudents([...Studentlist, newStudent]);
   }
+
+
   return (
     <>
-    <div className="bg-black-50 min-h-screen">
-      <main className="max-w-4xl p-8">
-        <div className="border-b border-black-400 pb-2 mb-4">
-          <h1 className="font-bold text-black-800">List of Students</h1>
-        </div>
-        <div className="flex flex-col gap-15">
-          <div className="felx flex-col gap-4">
+      <div className="bg-black-50 min-h-screen">
+        <main className="max-w-4xl p-8">
+          <h1 className="font-bold text-black-800 underline-offset-4 underline mb-4">List of Students</h1>
+          <div className="flex flex-row gap-4">
             {Studentlist.map((student) => (
-            <StudentCard key={student.id} first={student.firstName} last={student.lastName} dob={student.dob} grade={student.grade} />
-             ))}
+              <StudentCard key={student.id} first={student.firstName} last={student.lastName} dob={student.dob} grade={student.grade} />
+            ))}
+
           </div>
-          
-        </div>
-        <div className="border-b border-black-400 pb-2 mb-4">
-          <h2 className="font-bold text-black-800">Register a New Student</h2>
-         <FormDisplay addstudent={addstudent} />
-        </div>
-      </main>
-    </div>
-      
+          <h2 className="font-bold text-black-800 mt-4">Register a New Student</h2>
+          <FormDisplay addstudent={addstudent} />
+        </main>
+      </div>
+
     </>
   );
 }
