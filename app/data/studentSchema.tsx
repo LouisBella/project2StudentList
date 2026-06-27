7bb5fb01396a11871a5eb5fc4aee2d65b547a63f
Aiden-Larsen <aiden.larsen@edu.sait.ca>
2026-06-27 00:04:00 -0600

    fixed grade data validation

--- app/data/studentSchema.tsx
+++ app/data/studentSchema.tsx
@@ -4,7 +4,12 @@ export const studentSchema = z.object({
   firstName: z.string().min(1, "Please enter a Name"),
   lastName: z.string().min(1, "Please enter a Name"),
   dob: z.string().regex(/^\d{4}-\d{2}-\d{2}/, "Please use YYYY-MM-DD format"),
-  grade: z.string().min(1, "Enter grade of student")
+  grade: z
+    .string()
+    .refine((g) => Number(g) > 0, { message: "Please use valid grade (1-12)" })
+    .refine((g) => Number(g) < 13, {
+      message: "Please use valid grade (1-12)",
+    }),
 });
 
-export type StudentData = z.infer<typeof studentSchema>
+export type StudentData = z.infer<typeof studentSchema>;
